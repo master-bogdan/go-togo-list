ba3fefe2cc5527a5bf4c9409a7165b2899dae113
Bogdan <maiskiychavk@gmail.com>
2024-12-14 12:53:04 -0500

    add env

--- src/main.go
+++ src/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 type Todo struct {
@@ -15,6 +17,14 @@ type Todo struct {
 
 func main() {
 	var app = fiber.New()
+
+	var err = godotenv.Load("../.env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	var PORT = os.Getenv("PORT")
 
 	var todos = []Todo{}
 
@@ -67,5 +77,5 @@ func main() {
 		return c.Status(404).JSON(fiber.Map{"error": "todo not found"})
 	})
 
-	log.Fatal(app.Listen(":4000"))
+	log.Fatal(app.Listen(":" + PORT))
 }
